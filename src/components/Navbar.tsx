75b3fe2b1918b3d66f661578e5bacf44905e3505
Damilee2010 <rachaelfun10@gmail.com>
2025-11-21 17:41:19 +0100

    Initial commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -23,7 +23,7 @@ export const Navbar = ({ theme, toggleTheme }: NavbarProps) => {
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <a href="#home" className="text-2xl font-bold gradient-text">
-            DevPortfolio
+            Dammiee
           </a>
 
           {/* Desktop Navigation */}
